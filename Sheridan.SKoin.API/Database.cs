97178d173790a09c9c49a3c7943617ac90e56dc3
Lucida Dragon <lucidadragon@gmail.com>
2020-01-20 17:23:33 -0500

    Jank MySQL implementation, might replace.

---
+++ Sheridan.SKoin.API/Database.cs
@@ -0,0 +1,146 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace Sheridan.SKoin.API
+{
+    public static class Database
+    {
+        private static MySqlConnection Connection;
+
+        public static bool TryConnect()
+        {
+            try
+            {
+                var builder = new MySqlConnectionStringBuilder
+                {
+                    Server = "172.24.0.2",
+                    UserID = "skylar",
+                    Password = "A1@yeetme",
+                    Database = "SKoin"
+                };
+
+                Connection = new MySqlConnection(builder.ConnectionString);
+                Connection.Open();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"SQL Error: {ex.Message}");
+                return false;
+            }
+        }
+
+        public static bool TryCreateUser(string hash, out Guid user)
+        {
+            user = Guid.NewGuid();
+
+            try
+            {
+                using (var command = new MySqlCommand($"INSERT INTO Users (GUID, Password) VALUES ('{user}', '{hash}');", Connection))
+                {
+                    if (command.ExecuteNonQuery() > 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch { }
+
+            return false;
+        }
+
+        public static bool TryGetPassword(Guid user, out string hash)
+        {
+            try
+            {
+                using (var command = new MySqlCommand($"SELECT (Password) FROM Users WHERE GUID='{user}';", Connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        hash = reader.GetString(0);
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                hash = string.Empty;
+                return false;
+            }
+        }
+
+        public static bool TrySetPassword(Guid user, string hash)
+        {
+            try
+            {
+                using (var command = new MySqlCommand($"UPDATE Users SET Password='{hash}' WHERE GUID='{user}';", Connection))
+                {
+                    if (command.ExecuteNonQuery() > 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch { }
+
+            return false;
+        }
+
+        public static bool TryGetBalance(Guid user, out ulong balance)
+        {
+            try
+            {
+                using (var command = new MySqlCommand($"SELECT (Balance) FROM Users WHERE GUID='{user}';", Connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        balance = reader.GetUInt64(0);
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                balance = 0;
+                return false;
+            }
+        }
+
+        public static bool TryTransact(Guid fromUser, Guid toUser, ulong amount)
+        {
+            try
+            {
+                if (TryGetBalance(fromUser, out ulong balance) && TryGetBalance(toUser, out _))
+                {
+                    if (balance >= amount)
+                    {
+                        using (var transferCommand = new MySqlCommand($"UPDATE Users SET Balance=Balance-{amount} WHERE GUID='{fromUser}'; UPDATE Users SET Balance=Balance+{amount} WHERE GUID='{toUser}';", Connection))
+                        {
+                            if (transferCommand.ExecuteNonQuery() > 0)
+                            {
+                                try
+                                {
+                                    using (var logCommand = new MySqlCommand($"INSERT INTO Transactions (From, To, Amount) VALUES ('{fromUser}', '{toUser}', {amount});"))
+                                    {
+                                        transferCommand.ExecuteNonQuery();
+                                    }
+                                }
+                                catch { }
+
+                                return true;
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- Sheridan.SKoin.API/Program.cs
+++ Sheridan.SKoin.API/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Sheridan.SKoin.API.Core;
 using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Sheridan.SKoin.API
 {
@@ -12,6 +14,14 @@ namespace Sheridan.SKoin.API
         private static void Main(string[] args)
         {
             var port = GetSpecifiedPort(args);
+
+            Console.WriteLine("Connecting to database...");
+
+            if (!Database.TryConnect())
+            {
+                Console.WriteLine("Failed to connect to database.");
+                return;
+            }
 
             Server = new HttpServer();
 
@@ -30,7 +40,79 @@ namespace Sheridan.SKoin.API
             {
                 command = Console.ReadLine().ToLower().Trim();
 
-                if (command == "stop") break;
+                if (command == "stop")
+                {
+                    break;
+                }
+                else if (command == "newuser")
+                {
+                    Console.Write("Enter new password: ");
+
+                    if (Database.TryCreateUser(Convert.ToBase64String(
+                            SHA256.Create().ComputeHash(
+                                Encoding.UTF8.GetBytes(Console.ReadLine())
+                                )
+                            ), out Guid newUser))
+                    {
+                        Console.WriteLine($"User \"{newUser}\" created successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to create new user.");
+                    }
+                }
+
+                var parts = command.Split(' ');
+
+                if (parts.Length == 2 && Guid.TryParse(parts[1], out Guid user))
+                {
+                    if (parts[0] == "user")
+                    {
+                        if (Database.TryGetBalance(user, out ulong balance) && Database.TryGetPassword(user, out string hash))
+                        {
+                            Console.WriteLine(string.Empty.PadLeft(Console.BufferWidth, '='));
+                            Console.WriteLine($"User: {user}");
+                            Console.WriteLine($"Balance: {balance}");
+                            Console.WriteLine($"Password Hash: {hash}");
+                            Console.WriteLine(string.Empty.PadLeft(Console.BufferWidth, '='));
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Entry not found for user \"{user}\".");
+                        }
+                    }
+                    else if (parts[0] == "passwd")
+                    {
+                        Console.Write("Enter new password: ");
+
+                        if (Database.TrySetPassword(user, Convert.ToBase64String(
+                            SHA256.Create().ComputeHash(
+                                Encoding.UTF8.GetBytes(Console.ReadLine())
+                                )
+                            )))
+                        {
+                            Console.WriteLine("Password has been changed.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Entry not found for user \"{user}\".");
+                        }
+                    }
+                }
+                else if (parts.Length == 4 && Guid.TryParse(parts[1], out Guid from) && Guid.TryParse(parts[2], out Guid to) && ulong.TryParse(parts[3], out ulong amount))
+                {
+                    if (parts[0] == "transfer")
+                    {
+                        if (Database.TryTransact(from, to, amount))
+                        {
+                            Console.WriteLine("Transaction completed.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Failed to transfer {amount} from \"{from}\" to \"{to}\".");
+                        }
+                    }
+                }
             }
 
             Console.WriteLine("Stopping server...");
